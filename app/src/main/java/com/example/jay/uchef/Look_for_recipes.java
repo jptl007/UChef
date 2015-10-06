92aadc966ebb05d74051b93975aaf915935afdb6
JAY PATEL <jptl007@gmail.com>
2015-10-06 23:05:17 +1000

    "L"ook for recipe java file
    Look_for_recipe latest code
    
    do not consider "look_for_recipe"

--- app/src/main/java/com/example/jay/uchef/Look_for_recipes.java
+++ app/src/main/java/com/example/jay/uchef/Look_for_recipes.java
@@ -14,15 +14,72 @@ import android.widget.TextView;
 public class Look_for_recipes extends Activity {
 
     TextView cat_browse;
+    ImageButton asian;
+    ImageButton american;
+    ImageButton indian;
+    ImageButton italian;
+    ImageButton mexican;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.look_for_recipes);
 
+        asian = (ImageButton) findViewById(R.id.asian_btn);
+        american = (ImageButton) findViewById(R.id.american_btn);
+        indian = (ImageButton) findViewById(R.id.indian_btn);
+        italian = (ImageButton) findViewById(R.id.italian_btn);
+        mexican = (ImageButton) findViewById(R.id.mexican_btn);
+
+
         cat_browse = (TextView) findViewById(R.id.browse_cat);
         Typeface face = Typeface.createFromAsset(getAssets(), "fonts/Avalon.ttf");
         cat_browse.setTypeface(face);
+
+        asian.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent asian = new Intent(Look_for_recipes.this, Vegetable_cat.class);
+                startActivity(asian);
+
+            }
+        });
+
+        american.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent american = new Intent(Look_for_recipes.this, Vegetable_cat.class);
+                startActivity(american);
+
+            }
+        });
+
+        indian.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent indian = new Intent(Look_for_recipes.this, Vegetable_cat.class);
+                startActivity(indian);
+
+            }
+        });
+
+        italian.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent italian = new Intent(Look_for_recipes.this, Vegetable_cat.class);
+                startActivity(italian);
+
+            }
+        });
+
+        mexican.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mexican = new Intent(Look_for_recipes.this, Vegetable_cat.class);
+                startActivity(mexican);
+
+            }
+        });
     }
 
 }
