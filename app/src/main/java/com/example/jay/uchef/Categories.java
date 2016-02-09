8491284608b9d2d2a9ed1efd10c27f91cef2b1a2
hari-ladva <hari.ladva@yahoo.com>
2016-02-10 00:09:56 +1000

    Categories
    DB

--- app/src/main/java/com/example/jay/uchef/Categories.java
+++ app/src/main/java/com/example/jay/uchef/Categories.java
@@ -1,18 +1,16 @@
-package com.example.jay.uchef;
+package in.ladvas.uchef;
+
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
-/**
- * Created by JAY on 8/25/2015.
- */
+
 public class Categories extends Activity {
+    DBHelper myDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +19,22 @@ public class Categories extends Activity {
         ImageButton fruit;
         ImageButton dairy;
         ImageButton meat;
+        ImageButton seafood;
+        ImageButton herbs;
+        ImageButton bakery;
+
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.categories);
+
 
         veg = (ImageButton) findViewById(R.id.vegetable);
         fruit = (ImageButton) findViewById(R.id.fruit);
         dairy = (ImageButton) findViewById(R.id.dairy);
         meat = (ImageButton) findViewById(R.id.meat);
+        seafood = (ImageButton) findViewById(R.id.sea_food);
+        herbs = (ImageButton) findViewById(R.id.herbs);
+        bakery = (ImageButton) findViewById(R.id.bakery);
 
         veg.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,5 +71,37 @@ public class Categories extends Activity {
 
             }
         });
+
+        seafood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent meat = new Intent(Categories.this, Seafood_cat.class);
+                startActivity(meat);
+
+            }
+        });
+        herbs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent herb = new Intent(Categories.this, Herbs_cat.class);
+                startActivity(herb);
+
+            }
+        });
+        bakery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent bak = new Intent(Categories.this, Bakery_cat.class);
+                startActivity(bak);
+
+            }
+        });
     }
+
+
+    @Override
+        protected void onDestroy(){
+        super.onDestroy();
+    
+           }
 }
